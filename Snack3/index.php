db7921eb51be430c1c6c761a00cb77f4ed48f2be
Daniel <angelicodaniel93@gmail.com>
2022-12-13 15:47:11 +0100

    pushatina

--- Snack3/index.php
+++ Snack3/index.php
@@ -1,1 +1,52 @@
-<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+<!-- Creare un array di array. 
+Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
+Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Marco Rossi',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Paolo Bianchi',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Carcarlo Pravettoni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Giacomo Tafazzi',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Hulk Hogan',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+for ($i=0; $i < count($posts) ; $i++) { 
+    echo $posts[$i]['text'];
+};
+
+?>
--- Snack4/index.php
+++ Snack4/index.php
@@ -1,1 +1,20 @@
 <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php 
+
+$newArr = [];
+
+for ($i=0; $i < 15; $i++) { 
+
+    $randomNumber = rand(1, 100);
+
+    if (!in_array($randomNumber, $newArr)){        
+        $newArr[] = $randomNumber;
+    } else {
+        $i++
+    };
+};
+var_dump($newArr)
+
+
+?>
