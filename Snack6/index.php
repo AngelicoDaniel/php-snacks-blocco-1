67bf44d1436fcac6224739ad0da09203275aeaf7
Daniel <angelicodaniel93@gmail.com>
2022-12-13 16:38:08 +0100

    inizio snack 7

--- Snack6/index.php
+++ Snack6/index.php
@@ -1,1 +1,57 @@
-<!-- Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+<!-- Utilizzare questo array: https://pastebin.com/CkX3680A. 
+Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+<?php
+ 
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+ 
+
+    // echo "<pre>";
+    // var_dump( $db );
+    // echo "</pre>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div style="background-color:red;">
+    <?php  
+    for ($i=0; $i < count($db[0]); $i++) { 
+        echo $db['teachers']['name'];
+    }
+    ?>
+    </div>
+
+    <div style="background-color:green;">
+
+    </div>
+</body>
+</html>
--- Snack7/index.php
+++ Snack7/index.php
@@ -1,1 +1,37 @@
-<!-- Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+<!-- Creare un array contenente qualche alunno di un’ipotetica classe. 
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php
+
+$studenti = [
+    [
+        'nome' => 'Paolo',
+        'cognome' => 'Rossi',
+        'voti' => [7, 8, 3, 5, 6]
+    ],
+    [
+        'nome' => 'Roberto',
+        'cognome' => 'Baggio',
+        'voti' => [7, 2, 5, 9, 6]
+    ],   
+    [
+        'nome' => 'Gianni',
+        'cognome' => 'Rivera',
+        'voti' => [1, 8, 3, 3, 10]
+    ],   
+    [
+        'nome' => 'Jose',
+        'cognome' => 'Altafini',
+        'voti' => [6, 6, 9, 2, 4]
+    ],
+
+
+];
+
+for ($i=0; $i < count($studenti); $i++) { 
+    echo '<br>';
+    echo $studenti[$i]['nome'] . ' ' . $studenti[$i]['cognome'] ;
+}
+
+?>
