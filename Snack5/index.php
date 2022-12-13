4a8daa97708fe0630ffbfb3c8c8a485744ebddf0
Daniel <angelicodaniel93@gmail.com>
2022-12-13 16:07:44 +0100

    sncak5

--- Snack5/index.php
+++ Snack5/index.php
@@ -1,1 +1,26 @@
-<!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+<!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi.
+Prendere il paragrafo e suddividerlo in tanti paragrafi. 
+Ogni punto un nuovo paragrafo. -->
+
+
+<?php 
+
+$testo = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia odit fuga natus ullam maxime architecto itaque minima perspiciatis asperiores, minus officiis eos voluptatum facilis nisi ipsam laudantium doloribus modi corrupti.
+Nam hic, reiciendis magni eligendi corporis sapiente, ipsam explicabo modi quod impedit amet, eos eius? Aut blanditiis labore aliquid, sunt quasi omnis fugiat at animi nemo quisquam natus, assumenda cumque.
+Maxime, aliquid vel officiis harum qui laudantium. Iste doloremque fugiat vero repellendus ipsam rerum alias ut repudiandae asperiores quasi sunt minus autem eius, atque quae architecto animi quod in quia!';
+
+$paragrafo = explode('.', $testo);
+
+for ($i=0; $i < count($paragrafo); $i++) { 
+    echo '<br>';
+    echo $paragrafo[$i];
+}
+
+
+
+
+
+
+
+
+?>
